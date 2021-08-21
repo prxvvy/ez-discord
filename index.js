996e77bca3782455b8cf5ebce993a0ba15b79f58
prxvvy <61836780+prxvvy@users.noreply.github.com>
2021-08-21 13:56:56 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+const Client_1 = require("./src/client/Client");
+const Command_1 = require("./src/client/Command");
+const Event_1 = require("./src/client/Event");
+module.exports = {
+    Client: Client_1.Client,
+    Command: // The client class.
+    Command_1.Command,
+    Event: // The command class.
+    Event_1.Event, // The event class.
+};
